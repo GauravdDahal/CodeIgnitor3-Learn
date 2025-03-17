1f17c330c85ae181b1d0aabf164af4276fcceed8
GauravdDahal <Upworkgaurav555@gmail.com>
2025-03-17 14:16:34 +0545

    Create Custom library in code ignitor

---
+++ Customlibrary.php
@@ -0,0 +1,11 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Example{ //change according to file name
+    public function Test(){
+
+        echo "This is custom library";
+    }
+}
+
+?>
